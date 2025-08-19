e092b67866dc8d71f06a95f925ef1ae1b775910f
Carlos Vega Instipp <carlos.vega@instipp.edu.ec>
2025-08-18 23:21:34 -0500

    49. Component Composition: The special children

---
+++ src/components/TabButton.jsx
@@ -0,0 +1,15 @@
+// export default function TabButton(props) {
+//   return (
+//     <li>
+//       <button>{props.children}</button>
+//     </li>
+//   );
+// }
+
+export default function TabButton({ children }) {
+  return (
+    <li>
+      <button>{children}</button>
+    </li>
+  );
+}
