78d4675562d23b12db9566bd8bd275040050ab63
Carlos Vega Instipp <carlos.vega@instipp.edu.ec>
2025-08-26 17:50:19 -0500

    50. Reacting to Events [Core Concept]

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -7,9 +7,14 @@
 // }
 
 export default function TabButton({ children }) {
+
+  function handleClick() {
+    console.log('Hello World!');
+  }
+
   return (
     <li>
-      <button>{children}</button>
+      <button onClick={handleClick}>{children}</button>
     </li>
   );
 }
