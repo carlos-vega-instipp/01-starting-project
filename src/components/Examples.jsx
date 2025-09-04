51620362aafa4dbda1b9731baf3d15070ff4b52b
Carlos Vega Instipp <carlos.vega@instipp.edu.ec>
2025-09-03 20:56:19 -0500

    64. Splitting Components By Feature & State

---
+++ src/components/Examples.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import TabButton from "./TabButton.jsx";
+import { EXAMPLES } from "../data.js";
+
+export default function Examples() {
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  function handleSelect(selectedButton) {
+    // selectedButton => 'Components', 'JSX', 'Props', 'States'
+    //selectedTab = selectedButton;
+    setSelectedTopic(selectedButton);
+  }
+
+  // console.log("selectedTopic", selectedTopic);
+  return (
+    <section id="examples">
+      <h2>Examples</h2>
+      <menu>
+        <TabButton
+          isSelected={selectedTopic === "components"}
+          onSelect={() => handleSelect("components")}
+        >
+          Components
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "jsx"}
+          onSelect={() => handleSelect("jsx")}
+        >
+          JSX
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "props"}
+          onSelect={() => handleSelect("props")}
+        >
+          Props
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "state"}
+          onSelect={() => handleSelect("state")}
+        >
+          State
+        </TabButton>
+      </menu>
+      {!selectedTopic && <p>Por favor selecciona un contenido</p>}
+      {selectedTopic && (
+        <div id="tab-content">
+          <h3>{EXAMPLES[selectedTopic].title}</h3>
+          <p>{EXAMPLES[selectedTopic].description}</p>
+          <pre>
+            <code>{EXAMPLES[selectedTopic].code}</code>
+          </pre>
+        </div>
+      )}
+    </section>
+  );
+}
